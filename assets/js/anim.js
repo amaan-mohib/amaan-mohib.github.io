e8ecd8fe20cc28e76702a3c14e636fe67d7826ef
amaan-mohib <amaan.mohib@gmail.com>
2020-05-02 13:50:18 +0530

    first commit

---
+++ assets/js/anim.js
@@ -0,0 +1,16 @@
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+AOS.init({
+  duration: 750
+});
